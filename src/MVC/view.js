98c2efcd72c7cda08d5840062eba341d06c4ad56
Andrii Reverchuk <andrejrevercuk@gmail.com>
2021-06-24 16:43:41 +0300

    add data-attribute for board

--- src/MVC/view.js
+++ src/MVC/view.js
@@ -1,3 +1,35 @@
 export class View {
-   constructor()
+   constructor() {
+      console.log("view");
+      console.log(this.getElements("tr"));
+      this.renderTable();
+   }
+   renderTable() {
+      const firstChild = this.getElements("tr>td:first-child");
+      const rowElemets = this.getElements("tr");
+      const rowIndex = this.getElements("tr:last-child>td>span");
+      for (let i = 0; i < firstChild.length; i++) {
+         console.log(rowIndex[i].textContent);
+         const p = document.createElement("p");
+         p.textContent = i + 1;
+         const content = firstChild[i].textContent;
+         firstChild[i].prepend(p);
+         rowElemets[i].setAttribute("data-row", i + 1);
+         const rowChildElements = rowElemets[i].childNodes;
+         let counter = 0;
+         rowChildElements.forEach((element) => {
+            if (element.nodeName !== "#text") {
+               element.setAttribute(
+                  "data-column",
+                  rowIndex[counter].textContent
+               );
+               counter++;
+            }
+         });
+      }
+   }
+   getElements(selector) {
+      const elem = document.querySelectorAll(selector);
+      return elem;
+   }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Molel } from "./MVC/model";
+import { Model } from "./MVC/model";
 import { View } from "./MVC/view";
 import { Controller } from "./MVC/controller";
 const app = new Controller(new Model(), new View());
